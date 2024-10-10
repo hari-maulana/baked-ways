2b97c0325866f2045f4522fe76d685beaa73c3a6
maulanahari <harimaulana.xyz@gmail.com>
2024-10-10 18:36:36 +0700

    Add navigate to auth when not logged in

--- client/src/components/commons/Navigation.tsx
+++ client/src/components/commons/Navigation.tsx
@@ -1,6 +1,14 @@
 import { Icon } from "@iconify/react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-export const Navigation = () => {
+interface NavigationProps {
+  isLogin: boolean;
+}
+
+export const Navigation: React.FC<NavigationProps> = ({ isLogin }) => {
+  const navigate = useNavigate();
+
   return (
     <>
       {/* top nav */}
@@ -15,17 +23,25 @@ export const Navigation = () => {
 
         {/* right nav */}
         <div className="flex items-center justify-center h-[100%] gap-2">
-          <Icon icon="solar:cart-3-linear" className="w-8 h-8" />
-
-          <img
-            className="w-10 h-10 rounded-full"
-            src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-            alt="Rounded avatar"
-          />
-
-          {/* <button className="bg-gray-800 px-4 py-2 hover:bg-gray-900 rounded-lg">
-            <p className="text-white text-sm font-bold">LOGIN / SIGN UP</p>
-          </button> */}
+          {isLogin ? (
+            <>
+              <Icon icon="solar:cart-3-linear" className="w-8 h-8" />
+              <img
+                className="w-10 h-10 rounded-full"
+                src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+                alt="Rounded avatar"
+              />
+            </>
+          ) : (
+            <>
+              <button
+                onClick={() => navigate("/auth")}
+                className="bg-gray-800 px-4 py-2 hover:bg-gray-900 rounded-lg"
+              >
+                <p className="text-white text-sm font-bold">LOGIN / SIGN UP</p>
+              </button>
+            </>
+          )}
         </div>
       </div>
     </>
--- client/src/layout/RootLayout.tsx
+++ client/src/layout/RootLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Navigation } from "../components/commons/Navigation";
+import { useState } from "react";
 
 const RootLayout = () => {
+  const [isLogin, setIsLogin] = useState(true);
   return (
     <>
-      <Navigation />
+      <Navigation isLogin={isLogin} />
       <Outlet />
     </>
   );
