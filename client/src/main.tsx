ecff5c0639f050c6f1f18c1792aa74750e17f6b7
maulanahari <harimaulana.xyz@gmail.com>
2024-10-12 11:24:57 +0700

    add redux provider in main App

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,13 +3,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./store.ts";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ReduxProvider store={store}>
+        <App />
+      </ReduxProvider>
     </QueryClientProvider>
   </StrictMode>
 );
