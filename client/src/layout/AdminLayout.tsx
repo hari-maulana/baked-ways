b0b1f7472ae2ab8c75e30daab487527439e1e90c
maulanahari <harimaulana.xyz@gmail.com>
2024-10-15 20:18:20 +0700

    Refetch the user

--- client/src/layout/AdminLayout.tsx
+++ client/src/layout/AdminLayout.tsx
@@ -1,19 +1,45 @@
 import { useEffect } from "react";
 import { AdmNavbar } from "../components/admin/AdmNavbar";
 import { Outlet, useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../store";
+import { setUserProfile } from "../features/userProfile/userProfileSlice";
+import axios from "axios";
 
 export const AdminLayout = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const token = localStorage.getItem("token");
+  const userId = localStorage.getItem("userId");
 
   const { isLogin } = useSelector((state: RootState) => state.auth);
 
   useEffect(() => {
-    if (!isLogin) {
+    const fetchUserProfile = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3003/user/${userId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const profileData = response.data;
+        dispatch(setUserProfile(profileData));
+      } catch (error) {
+        console.error("Error fetching user profile:", error);
+      }
+    };
+
+    if (isLogin) {
+      fetchUserProfile();
+    } else {
+      // Navigate to the landing page if not logged in
       navigate("/landing-page");
     }
   }, [isLogin, navigate]);
+
   return (
     <>
       <AdmNavbar />
