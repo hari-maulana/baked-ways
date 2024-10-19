58a047194e9b0c3881c8f5b1a8ec955f61b9f4f1
maulanahari <harimaulana.xyz@gmail.com>
2024-10-19 23:41:18 +0700

    Fix cartlist to receive props

--- client/src/components/cartPage/ProductListCart.tsx
+++ client/src/components/cartPage/ProductListCart.tsx
@@ -1,23 +1,36 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
+interface ProductListCartProps {
+  image: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
 
-export const ProductListCart = () => {
+export const ProductListCart: React.FC<ProductListCartProps> = ({
+  image,
+  name,
+  price,
+  quantity,
+}) => {
   return (
     <>
       <li>
-        <div className="flex border-t-2 border-gray-500 py-2 justify-between">
+        <div className="flex bg-white rounded-lg border-b-2 border-gray-400 py-2 px-3 justify-between mb-3">
           <div className="flex">
             <img
               className="h-20 w-20 rounded-md"
-              src="https://assets.makobakery.com/cdn/web/product/1669019447_rendang-floss.JPG"
+              src={image}
               alt="product image"
             />
             <div className="ms-4">
-              <p>Rendang Floss</p>
-              <div className="flex gap-2">
+              <p>{name}</p>
+              <div className="flex gap-2 mt-3">
                 <button className="text-red-500 w-4 font-bold hover:bg-red-50  transition-all duration-100 ease-in-out  active:bg-gray-400 active:scale-95">
                   <Icon icon="ic:baseline-minus" />
                 </button>
-                <p className=" text-center bg-gray-200 px-2 rounded-sm">1</p>
+                <p className=" text-center bg-gray-200 px-2 rounded-sm">
+                  {quantity}
+                </p>
                 <button className="text-green-500 w-4 font-bold hover:bg-green-50 transition-all duration-100 ease-in-out  active:bg-gray-400 active:scale-95">
                   <Icon icon="ic:baseline-plus" />
                 </button>
@@ -25,7 +38,7 @@ export const ProductListCart = () => {
             </div>
           </div>
           <div className="flex flex-col items-center justify-between">
-            <p>7.000</p>
+            <p>{price}</p>
             <button className="mt-2 text-red-500 font-bold hover:text-red-700 transition-all duration-100 ease-in-out active:text-gray-400 active:scale-95">
               <Icon icon="mdi:trash-can-outline" fontSize={25} />
             </button>
